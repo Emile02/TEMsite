4d5d0a90fbc433cb49c506dd01ad1ba8c1488fd7
Emile Corvini <emile.corvini@epitech.eu>
2023-09-28 19:19:03 +0200

    delete previous architecture

--- BACKEND/models/tem.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const TemSchema = new mongoose.Schema({
-  email: String,
-});
-
-const Tem = mongoose.model('Tem', TemSchema);
-
-module.exports = Tem;
--- FRONTEND/src/components/LogoPartenaires.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="logoPartenaires w-full h-40 bg-white flex justify-center items-center">
-        <h1 class="text-black">Logo Partenaires</h1>
-    </div>
-</template>
