bd315f3fb5029ef2054a70f1d30a8720f9e42b63
Emile Corvini <emile.corvini@epitech.eu>
2023-12-12 23:54:42 +0100

    build css accueil evenement

--- FRONTEND/src/components/AccueilEvenement.vue
+++ FRONTEND/src/components/AccueilEvenement.vue
@@ -1,20 +1,19 @@
 <template>
     <div class="presentation flex flex-col md:flex-row-reverse mt-10">
-      <div class="md:mb-10 lg:mb-20 -mt-4 md:mt-10 lg:mt-20 w-full md:w-1/2 lg:h-[45rem] mb-10">
-          <div>
-              <h1 class="pt-5 text-xl md:text-2xl  lg:pt-40 font-light text-center md:text-left uppercase pl-5 md:pl-10  text-black  2xl:pt-42">ORGANISATION D'EVENEMENTS</h1>
-              <p class="pt-5 pl-5 md:pl-10 text-black font-light text-md text-justify pr-5 md:pr-28 tracking-widest">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur aspernatur voluptas <span class="font-bold">expedita dicta</span>, soluta repudiandae magnam beatae, iure magni cupiditate <span class="font-bold">suscipit</span> veritatis laboriosam asperiores qui exercitationem. Alias sequi itaque tempora. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et temporibus enim provident quis quae aut, eum nostrum corporis magnam perspiciatis sed labore dolor sapiente necessitatibus non possimus harum <span class="font-bold">quaerat esse!</span></p>
-              <div class="flex align-items mt-10 pl-5 md:pl-10 mb-2">
-                  <img src="../../public/LOGO_NOIR.svg" alt="LOGO COMPLET LE TEM" class="h-full max-h-10">
-                  <button>
-                    <h1 class="text-lg pl-2 font-light uppercase text-tem-yellow"><a href="https://drdh.fr/letem54/evenements-restaurant-le-tem" data-replace="Nos Evènements" class="event mt-2 text-lg font-light uppercase"><span>Nos Evènements</span></a></h1>
-                  </button>
-              </div>
-          </div>
-      </div>
+      <div class="md:mb-10 lg:mb-20 -mt-4 md:mt-10 lg:mt-8 w-full md:w-1/2 lg:h-[45rem] mb-10 sm:bg-transparent ">
+        <div class="">
+            <h1 class="pt-5 text-xl md:text-2xl lg:pr-20 lg:pt-40 font-light uppercase pl-5 md:pl-10 lg:pl-20 text-black xl:pl-32 2xl:pl-60 2xl:pr-60 2xl:pt-42">Organisation d'Evenements</h1>
+            <p class="pt-5 pl-5 md:pl-10 lg:pl-20 text-black text-justify pr-5 md:pr-10 lg:pr-20 xl:pl-32 xl:pr-32 2xl:pl-60 2xl:pr-60">Chez nous, la fraîcheur et la qualité des produits sont nos priorités. C'est pourquoi nous travaillons exclusivement avec des produits frais, de saison et provenant de producteurs locaux renommés. Nous sommes fiers de collaborer avec ces producteurs régionaux de confiance, tels que le maraîcher Haraux, le poulailler de Moulnot, Gourmet Food, la ferme de Crévic et la Maison Masse. Ces partenariats nous permettent de garantir des produits de la plus haute qualité à nos clients, tout en soutenant l'économie locale.</p>
+            <div class="flex align-items mt-10 pl-5 md:pl-10 lg:pl-20 xl:pl-32 2xl:pl-60 mb-7">
+                <img src="../../public/LOGO_NOIR.svg" alt="LOGO COMPLET LE TEM" class="h-full max-h-10">
+                <button>
+                  <h1 class="text-lg pl-2 font-light uppercase text-tem-yellow"><a href="https://drdh.fr/letem54/cartes-menu-restaurant-le-TEM" class="cartes" data-replace="Nos Cartes & Menus"><span>Nos Cartes & Menus</span></a></h1>
+                </button>
+            </div>
+        </div>
+    </div>
       <div class="flex md:mt-[4.4rem] lg:mt-[8rem] md:w-2/3">
         <div class="w-1/12 h-80 md:h-[25rem] lg:h-[40rem] bg-white flex justify-center items-center">
-          
         </div>
         <div class="w-10/12 h-80 md:h-[25rem] lg:h-[40rem] bg-red-200 mb-7">
             <img class="image-to-change w-full h-full object-cover" src="https://drdh.fr/4I8A4099.jpg" alt="Image 1">
