51f077b6b68ffa30eb3ca813ccc806e94325e105
Emile Corvini <emile.corvini@epitech.eu>
2023-12-13 00:45:44 +0100

    change background color

--- FRONTEND/src/components/AccueilEvenement.vue
+++ FRONTEND/src/components/AccueilEvenement.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="presentation flex flex-col md:flex-row-reverse mt-10">
-      <div class="md:mb-10 lg:mb-20 -mt-4 md:mt-10 lg:mt-8 w-full md:w-1/2 lg:h-[45rem] mb-10 sm:bg-transparent ">
+      <div class="md:mb-10 lg:mb-20 -mt-4 md:mt-10 lg:mt-8 w-full md:w-1/2 lg:h-[45rem] mb-10 md:bg-zinc-800  ">
         <div class="">
             <h1 class="pt-5 text-xl md:text-2xl lg:pr-20 lg:pt-40 font-light uppercase pl-5 md:pl-10 lg:pl-20 text-black xl:pl-32 2xl:pl-60 2xl:pr-60 2xl:pt-42">Organisation d'Evenements</h1>
             <p class="pt-5 pl-5 md:pl-10 lg:pl-20 text-black text-justify pr-5 md:pr-10 lg:pr-20 xl:pl-32 xl:pr-32 2xl:pl-60 2xl:pr-60">Notre restaurant s’inscrit depuis son ouverture dans un accueil tout particulier aux <strong class="font-bold">cocktails dînatoire</strong> pour les <strong class="font-bold">pots de thèses</strong> célébrants les fins d’études des jeunes diplômés. Nous proposons une déclinaison totale de notre carte (en <strong class="font-bold">accord dans la saison</strong>, avec les produits frais que nous travaillons à l’année) en version individuelle.</p>
